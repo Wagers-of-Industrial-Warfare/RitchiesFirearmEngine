6d906bfb8f404acc58fb16baf54c3e3c6d5da8c7
rbasamoyai <b33gy0sh@gmail.com>
2024-07-25 19:11:59 -0400

    feature: Added mod dependency handling to pack loading

--- src/main/java/rbasamoyai/ritchiesfirearmengine/pack_content/content_creation/plugins/RFEPluginManager.java
+++ src/main/java/rbasamoyai/ritchiesfirearmengine/pack_content/content_creation/plugins/RFEPluginManager.java
@@ -1,8 +1,8 @@
 package rbasamoyai.ritchiesfirearmengine.pack_content.content_creation.plugins;
 
-import net.minecraftforge.fml.ModList;
 import rbasamoyai.ritchiesfirearmengine.RitchiesFirearmEngine;
 import rbasamoyai.ritchiesfirearmengine.pack_content.resources.RFEPackMetadata;
+import rbasamoyai.ritchiesfirearmengine.utils.RFEModUtils;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,7 +15,7 @@ public class RFEPluginManager {
         Map<String, RFEPlugin> foundPlugins = new LinkedHashMap<>();
         for (RFEPlugin.Info info : metadata.pluginInfo()) {
             if (!PLUGINS.containsKey(info.classPath()))
-                foundPlugins.put(info.classPath(), loadPlugin(packId, info));
+                foundPlugins.put(info.classPath(), loadPlugin(packId, info, metadata));
         }
         for (RFEPlugin plugin : foundPlugins.values()) {
             plugin.registerItemBuilders();
@@ -23,22 +23,27 @@ public class RFEPluginManager {
         PLUGINS.putAll(foundPlugins);
     }
 
-    private static RFEPlugin loadPlugin(String packId, RFEPlugin.Info info) throws Exception {
+    private static RFEPlugin loadPlugin(String packId, RFEPlugin.Info info, RFEPackMetadata metadata) throws Exception {
         try {
-            if (!isModPresent(info.modId()))
+            if (!RFEModUtils.isModPresent(info.modId()))
                 throw new IllegalStateException("Plugin requires mod " + info.modId());
+            boolean dependenciesMatch = false;
+            for (RFEPackMetadata.DependencyInfo dependency : metadata.dependencies()) {
+                if (dependency.modId().equals(info.modId())) {
+                    dependenciesMatch = true;
+                    break;
+                }
+            }
+            if (!dependenciesMatch)
+                throw new IllegalStateException("Plugin's mod requirement is not present in pack metadata dependencies section");
             Class<?> clazz = Class.forName(info.classPath());
             if (!RFEPlugin.class.isAssignableFrom(clazz))
-                throw new IllegalStateException("Plugin class " + info.classPath() + " in mod " + info.modId() + " must implement RFEPlugin");
+                throw new IllegalStateException("Plugin must implement RFEPlugin");
             return (RFEPlugin) clazz.getDeclaredConstructor().newInstance();
         } catch (Exception t) {
-            RitchiesFirearmEngine.LOGGER.error("Error loading plugin class {} from source {}", info.classPath(), packId);
+            RitchiesFirearmEngine.LOGGER.error("Error loading plugin class {} of mod {} from source {}", info.classPath(), info.modId(), packId);
             throw t;
         }
-    }
-
-    private static boolean isModPresent(String modId) {
-        return ModList.get().getModFileById(modId) != null;
     }
 
     private RFEPluginManager() {}
--- src/main/java/rbasamoyai/ritchiesfirearmengine/pack_content/resources/RFEPackMetadata.java
+++ src/main/java/rbasamoyai/ritchiesfirearmengine/pack_content/resources/RFEPackMetadata.java
@@ -5,11 +5,13 @@ import com.google.gson.JsonObject;
 import net.minecraft.server.packs.metadata.MetadataSectionSerializer;
 import net.minecraft.util.GsonHelper;
 import rbasamoyai.ritchiesfirearmengine.pack_content.content_creation.plugins.RFEPlugin;
+import rbasamoyai.ritchiesfirearmengine.utils.RFEModUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public record RFEPackMetadata(String namespace, String displayName, List<RFEPlugin.Info> pluginInfo) {
+public record RFEPackMetadata(String namespace, String displayName, List<DependencyInfo> dependencies,
+                              List<RFEPlugin.Info> pluginInfo) {
     public static final Serializer TYPE = new Serializer();
 
     public static class Serializer implements MetadataSectionSerializer<RFEPackMetadata> {
@@ -20,17 +22,33 @@ public record RFEPackMetadata(String namespace, String displayName, List<RFEPlug
             String namespace = GsonHelper.getAsString(obj, "namespace");
             String displayName = GsonHelper.getAsString(obj, "display_name");
 
+            List<DependencyInfo> allDependencies = new ArrayList<>();
+            JsonArray dependencyJson = GsonHelper.getAsJsonArray(obj, "dependencies", new JsonArray());
+            int dependencySize = dependencyJson.size();
+            String versionField = RFEModUtils.getModVersionSpecifier();
+            for (int i = 0; i < dependencySize; ++i) {
+                JsonObject pluginInfoObject = dependencyJson.get(i).getAsJsonObject();
+                String modId = GsonHelper.getAsString(pluginInfoObject, "mod");
+                // Version only loads the loader version based on the field
+                String version = GsonHelper.getAsString(pluginInfoObject, versionField);
+                allDependencies.add(new DependencyInfo(modId, version));
+            }
+
             List<RFEPlugin.Info> allPluginInfo = new ArrayList<>();
-            JsonArray plugins = GsonHelper.getAsJsonArray(obj, "plugins", new JsonArray());
-            for (int i = 0; i < plugins.size(); ++i) {
-                JsonObject pluginInfoObject = plugins.get(i).getAsJsonObject();
+            JsonArray pluginsJson = GsonHelper.getAsJsonArray(obj, "plugins", new JsonArray());
+            int pluginSize = pluginsJson.size();
+            for (int i = 0; i < pluginSize; ++i) {
+                JsonObject pluginInfoObject = pluginsJson.get(i).getAsJsonObject();
                 String modId = GsonHelper.getAsString(pluginInfoObject, "mod");
                 String classPath = GsonHelper.getAsString(pluginInfoObject, "class");
                 allPluginInfo.add(new RFEPlugin.Info(modId, classPath));
             }
 
-            return new RFEPackMetadata(namespace, displayName, allPluginInfo);
+            return new RFEPackMetadata(namespace, displayName, allDependencies, allPluginInfo);
         }
+    }
+
+    public record DependencyInfo(String modId, String version) {
     }
 
 }

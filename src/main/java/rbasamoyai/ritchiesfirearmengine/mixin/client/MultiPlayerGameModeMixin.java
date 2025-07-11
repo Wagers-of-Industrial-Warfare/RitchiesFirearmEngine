269e68fd3fd791052442af2d5f446e5de9fae498
rbasamoyai <b33gy0sh@gmail.com>
2025-07-11 00:16:49 -0400

    bug, incomplete: Added swing blocking, fixed left-clicking firearm hurting mobs by melee
    - TODO add logic to enable melee

---
+++ src/main/java/rbasamoyai/ritchiesfirearmengine/mixin/client/MultiPlayerGameModeMixin.java
@@ -0,0 +1,24 @@
+package rbasamoyai.ritchiesfirearmengine.mixin.client;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import rbasamoyai.ritchiesfirearmengine.builtin_content.content.firearms.IFirearmItem;
+
+@Mixin(MultiPlayerGameMode.class)
+public abstract class MultiPlayerGameModeMixin {
+
+    @WrapMethod(method = "attack")
+    private void ritchiesfirearmengine$attack(Player player, Entity target, Operation<Void> original) {
+        ItemStack mainhand = player.getMainHandItem();
+        if (mainhand.getItem() instanceof IFirearmItem)
+            return;
+        // TODO offhand and melee
+        original.call(player, target);
+    }
+
+}

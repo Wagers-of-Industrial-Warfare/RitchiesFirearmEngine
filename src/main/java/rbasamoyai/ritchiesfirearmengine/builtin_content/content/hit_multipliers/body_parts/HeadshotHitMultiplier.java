2ad285c6c10cc622c9845f629cdb26e991fc3587
rbasamoyai <b33gy0sh@gmail.com>
2025-07-30 17:42:59 -0400

    feature: Added headshot and headshot gore hit multipliers
    - Added #ritchiesfirearmengine:humanoid tag to enable headshots for entities that roughly follow humanoid form (head, not wide)

---
+++ src/main/java/rbasamoyai/ritchiesfirearmengine/builtin_content/content/hit_multipliers/body_parts/HeadshotHitMultiplier.java
@@ -0,0 +1,31 @@
+package rbasamoyai.ritchiesfirearmengine.builtin_content.content.hit_multipliers.body_parts;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.phys.EntityHitResult;
+import rbasamoyai.ritchiesfirearmengine.RitchiesFirearmEngine;
+import rbasamoyai.ritchiesfirearmengine.builtin_content.content.hit_multipliers.RFEHitMultiplier;
+import rbasamoyai.ritchiesfirearmengine.builtin_content.default_index.BuiltInRFEPlugin;
+import rbasamoyai.ritchiesfirearmengine.foundation.api.projectiles.RFEProjectileInstance;
+
+public record HeadshotHitMultiplier(float multiplier) implements RFEHitMultiplier {
+
+    private static final TagKey<EntityType<?>> HUMANOID = TagKey.create(Registries.ENTITY_TYPE, RitchiesFirearmEngine.resource("humanoid"));
+
+    @Override
+    public float multiplyDamage(Entity target, RFEProjectileInstance projectile, EntityHitResult hitResult, float damage) {
+        if (!target.getType().is(HUMANOID))
+            return damage;
+        double yHit = hitResult.getLocation().y;
+        double headHalf = target.getBbHeight() - target.getEyeHeight();
+        if (Math.abs(yHit - target.getEyeY()) <= headHalf)
+            damage *= this.multiplier;
+        return damage;
+    }
+
+    @Override public RFEHitMultiplier.Provider getProvider() { return BuiltInRFEPlugin.HitMultipliers.HEADSHOT; }
+    @Override public float getMultiplier() { return this.multiplier; }
+
+}
--- src/main/java/rbasamoyai/ritchiesfirearmengine/builtin_content/default_index/BuiltInRFEPlugin.java
+++ src/main/java/rbasamoyai/ritchiesfirearmengine/builtin_content/default_index/BuiltInRFEPlugin.java
@@ -18,6 +18,8 @@ import rbasamoyai.ritchiesfirearmengine.builtin_content.content.firearms.spread.
 import rbasamoyai.ritchiesfirearmengine.builtin_content.content.hit_multipliers.RFEHitMultiplier;
 import rbasamoyai.ritchiesfirearmengine.builtin_content.content.hit_multipliers.RFEHitMultiplierHandler;
 import rbasamoyai.ritchiesfirearmengine.builtin_content.content.hit_multipliers.armor_piercing.ArmorPiercingHitMultiplier;
+import rbasamoyai.ritchiesfirearmengine.builtin_content.content.hit_multipliers.body_parts.HeadshotHitMultiplier;
+import rbasamoyai.ritchiesfirearmengine.builtin_content.content.hit_multipliers.body_parts.HeadshotHitMultiplierGore;
 import rbasamoyai.ritchiesfirearmengine.builtin_content.content.projectiles.bullet.RFEBulletProjectileType;
 import rbasamoyai.ritchiesfirearmengine.foundation.api.content_creation.RFEContentBuilderRegistry;
 import rbasamoyai.ritchiesfirearmengine.foundation.api.content_creation.plugins.RFEPlugin;
@@ -208,9 +210,13 @@ public class BuiltInRFEPlugin implements RFEPlugin {
 
     public static class HitMultipliers {
         public static final RFEHitMultiplier.Provider ARMOR_PIERCING = ArmorPiercingHitMultiplier::new;
+        public static final RFEHitMultiplier.Provider HEADSHOT = HeadshotHitMultiplier::new;
+        public static final RFEHitMultiplier.Provider HEADSHOT_GORE = HeadshotHitMultiplierGore::new;
 
         public static void register() {
             RFEContentBuilderRegistry.registerHitMultiplierProvider(RitchiesFirearmEngine.resource("armor_piercing"), ARMOR_PIERCING);
+            RFEContentBuilderRegistry.registerHitMultiplierProvider(RitchiesFirearmEngine.resource("headshot"), HEADSHOT);
+            RFEContentBuilderRegistry.registerHitMultiplierProvider(RitchiesFirearmEngine.resource("headshot_gore"), HEADSHOT_GORE);
         }
     }
 

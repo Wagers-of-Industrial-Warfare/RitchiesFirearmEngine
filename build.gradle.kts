1a9e2ec27e36f21b62d6c0845888730ad9a0c69a
rbasamoyai <b33gy0sh@gmail.com>
2026-01-01 02:21:47 -0500

    buildscript: Add Machete to somewhat reduce the JAR size when built with CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id("dev.architectury.loom") version "1.11.+"
     id("maven-publish")
+    id("io.github.p03w.machete") version "2.0.1"
 }
+
+val ci = System.getenv("CI")?.toBoolean() ?: false
 
 base {
     archivesName.set(prop("mod_id"))
@@ -133,3 +136,6 @@ publishing {
     }
 }
 
+machete {
+    enabled = ci // Enable Machete only on CI
+}

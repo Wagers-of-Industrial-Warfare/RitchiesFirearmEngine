64ff35f533d0f66ca103c9337a04ebc1596cda19
rbasamoyai <b33gy0sh@gmail.com>
2026-08-22 23:31:54 -0400

    refactor: Changed suppressor attachment properties to use RFEFirearmProperties

--- src/main/java/rbasamoyai/ritchiesfirearmengine/builtin_content/content/item_attachments/supperssors/SuppressorAttachmentProperties.java
+++ src/main/java/rbasamoyai/ritchiesfirearmengine/builtin_content/content/item_attachments/supperssors/SuppressorAttachmentProperties.java
@@ -1,6 +1,5 @@
 package rbasamoyai.ritchiesfirearmengine.builtin_content.content.item_attachments.supperssors;
 
-import com.google.common.collect.ImmutableMap;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -10,17 +9,17 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import rbasamoyai.ritchiesfirearmengine.builtin_content.default_index.BuiltInRFEPlugin;
+import rbasamoyai.ritchiesfirearmengine.foundation.api.RFEFirearmProperties;
 import rbasamoyai.ritchiesfirearmengine.foundation.api.item_attachments.properties.RFEItemAttachmentProperties;
 
 import javax.annotation.Nullable;
-import java.util.HashMap;
 import java.util.Optional;
 
-public record SuppressorAttachmentProperties(SuppressedSound defaultSuppressedSound, ImmutableMap<String, SuppressedSound> suppressedSoundByMode)
+public record SuppressorAttachmentProperties(RFEFirearmProperties<SuppressedSound> soundProperties)
     implements RFEItemAttachmentProperties {
 
     public SuppressedSound getSound(String mode) {
-        return this.suppressedSoundByMode.getOrDefault(mode, this.defaultSuppressedSound);
+        return this.soundProperties.getProperties(mode);
     }
 
     @Override public boolean overridesDefaults() { return true; }
@@ -45,15 +44,11 @@ public record SuppressorAttachmentProperties(SuppressedSound defaultSuppressedSo
 
     public static class Serializer implements RFEItemAttachmentProperties.Serializer<SuppressorAttachmentProperties> {
         private static final MapCodec<SuppressorAttachmentProperties> CODEC = RecordCodecBuilder.mapCodec(o -> o.group(
-                SuppressedSound.CODEC.forGetter(SuppressorAttachmentProperties::defaultSuppressedSound),
-                Codec.unboundedMap(Codec.STRING, SuppressedSound.CODEC.codec()).xmap(ImmutableMap::copyOf, HashMap::new)
-                        .optionalFieldOf("modes", ImmutableMap.of()).forGetter(SuppressorAttachmentProperties::suppressedSoundByMode)
+                RFEFirearmProperties.makeCodec(SuppressedSound.CODEC).fieldOf("sound").forGetter(SuppressorAttachmentProperties::soundProperties)
         ).apply(o, SuppressorAttachmentProperties::new));
 
         private static final StreamCodec<RegistryFriendlyByteBuf, SuppressorAttachmentProperties> STREAM_CODEC = StreamCodec.composite(
-                SuppressedSound.STREAM_CODEC, SuppressorAttachmentProperties::defaultSuppressedSound,
-                ByteBufCodecs.map(HashMap::new, ByteBufCodecs.STRING_UTF8, SuppressedSound.STREAM_CODEC)
-                        .map(ImmutableMap::copyOf, HashMap::new), SuppressorAttachmentProperties::suppressedSoundByMode,
+                RFEFirearmProperties.makeStreamCodec(SuppressedSound.STREAM_CODEC), SuppressorAttachmentProperties::soundProperties,
                 SuppressorAttachmentProperties::new);
 
         @Override public MapCodec<SuppressorAttachmentProperties> codec() { return CODEC; }
